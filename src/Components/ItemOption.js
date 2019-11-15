900e15e16ace3b20f867096a6efddc115c6cd946
Jack <pupeljack@gmail.com>
2019-11-15 14:08:34 -0600

    fixed the issue of item options not showing up

--- src/Components/ItemOption.js
+++ src/Components/ItemOption.js
@@ -1,24 +1,20 @@
  import React from 'react';
 
  export default function ItemOption(props){
-    const { item, selected, featureTitle, handleUpdate } = props;
+    const { item, /*selected,*/ featureTitle, handleUpdate } = props;
   
-    const selectedClass = item.name === selected[featureTitle].name ? 'feature__selected' : '';
-    const featureClass = 'feature__option ' + selectedClass;
+    //const selectedClass = item.name === selected[featureTitle].name ? 'feature__selected' : '';
+    //const featureClass = 'feature__option ' + selectedClass;
 
     return (
         <li className="feature__item">
-            <div className={featureClass}>
-
-    
-    test
+            
+                <div onClick={() => handleUpdate(featureTitle, item)}>
+                    {item.name}
+                    ({new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+                        .format(item.cost)})
                 </div>
+            
         </li>
     );
  }
-
-            /* onClick={() => handleUpdate(featureTitle, item)}>
-                {item.name}
-                ({new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
-                    .format(item.cost)})
-    */
