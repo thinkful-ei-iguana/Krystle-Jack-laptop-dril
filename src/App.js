8a96a5863f62b155b0b48543af6f3a490eab8120
Jack <pupeljack@gmail.com>
2019-11-15 13:35:27 -0600

    did our best to understand the work of others, forms are not showing up yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Cart from './Components/Features'
+import SummarySection from './Components/SummarySection';
+import Header from './Components/Header';
+import FormSection from './Components/FormSection';
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
@@ -16,26 +18,30 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
 });
 
 class App extends Component {
-  state = {
-    selected: {
-      Processor: {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
-        cost: 700
-      },
-      'Operating System': {
-        name: 'Ubuntu Linux 16.04',
-        cost: 200
-      },
-      'Video Card': {
-        name: 'Toyota Corolla 1.5v',
-        cost: 1150.98
-      },
-      Display: {
-        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
+  constructor(props){
+    super(props);
+  
+    this.state = {
+      selected: {
+        Processor: {
+          name: '17th Generation Intel Core HB (7 Core with donut spare)',
+          cost: 700
+        },
+        'Operating System': {
+          name: 'Ubuntu Linux 16.04',
+          cost: 200
+        },
+        'Video Card': {
+          name: 'Toyota Corolla 1.5v',
+          cost: 1150.98
+        },
+        Display: {
+          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+          cost: 1500
+        }
       }
-    }
-  };
+    };
+  }
 
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
@@ -46,8 +52,16 @@ class App extends Component {
   };
 
   render() {
-    
-
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <FormSection selected={this.state.selected} handleUpdate={this.updateFeature} />
+          <SummarySection selected={this.state.selected} />
+        </main>
+      </div>
+    );
+/*
     const summary = Object.keys(this.state.selected).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
       const selectedOption = this.state.selected[feature];
@@ -90,7 +104,7 @@ class App extends Component {
           </section>
         </main>
       </div>
-    );
+    );*/
   }
 }
 
