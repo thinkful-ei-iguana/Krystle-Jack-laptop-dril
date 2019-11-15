9f31a1a0b5e9514a20db19f54ec4f5acf7f1ab3d
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2019-11-15 11:59:52 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cart from './Components/Cart'
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
---
+++ src/Components/Cart.js
@@ -0,0 +1,9 @@
+
+
+updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
